6779887efb3dea825852924b409489dbbb6c4ff0
steffychambi <steffychambi@gmail.com>
2026-07-11 18:48:24 -0400

    assignment week 01

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let today = new Date();
+
+document.getElementById("currentyear").textContent = today.getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
